2d485e41f3458040367abb953272a184adde32b5
Siergiej Riaguzow <sriaguzow@astek.pl>
2021-01-26 13:07:10 +0100

    remove dead code

--- home/ss/.config/awesome/rc_2_mon.lua
+++ home/ss/.config/awesome/rc_2_mon.lua
@@ -97,21 +97,6 @@ awful.layout.layouts = {
 }
 -- }}}
 
--- {{{ Helper functions
-local function client_menu_toggle_fn()
-    local instance = nil
-
-    return function ()
-        if instance and instance.wibox.visible then
-            instance:hide()
-            instance = nil
-        else
-            instance = awful.menu.clients({ theme = { width = 250 } })
-        end
-    end
-end
--- }}}
-
 -- {{{ Menu
 -- Create a launcher widget and a main menu
 myawesomemenu = {
